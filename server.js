cf6caa8d0a087d826bea767e027c8faab1159a32
mac <mac@macs-MacBook-Pro.local>
2025-06-18 03:09:06 -0700

    Update website

--- server.js
+++ server.js
@@ -77,7 +77,7 @@ await newBooking.save();
   // 2. Choose email based on location
   let recipientEmail = '';
   if (location === 'hh_towers') {
-    recipientEmail = 'trendynailsspothhtowers@gmail.com';
+    recipientEmail = 'trendynailspothhtowers@gmail.com';
   } else if (location === 'afya_center') {
     recipientEmail = 'josephmacharia286@gmail.com';
   } else {
